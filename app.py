9f07c40c5211bc706ebde347133d16a4904bc125
KevinWilmoth <kevin.wilmoth@gmail.com>
2022-07-07 19:54:09 -0400

    ENV VAR

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import flask
 from flask import request, jsonify
-import azure.cosmos.documents as documents
-import azure.cosmos.cosmos_client as cosmos_client
-import azure.cosmos.exceptions as exceptions
-from azure.cosmos.partition_key import PartitionKey
-import datetime
+#import azure.cosmos.documents as documents
+#import azure.cosmos.cosmos_client as cosmos_client
+#import azure.cosmos.exceptions as exceptions
+#from azure.cosmos.partition_key import PartitionKey
+#import datetime
 
 import config
 
