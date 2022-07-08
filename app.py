3f40810bb1b14055e55a37fb02bb0d088d4868c1
KevinWilmoth <kevin.wilmoth@gmail.com>
2022-07-07 20:37:43 -0400

    DB TRY 1555

--- app.py
+++ app.py
@@ -21,26 +21,26 @@ def home():
     return '<h1>Testing Cosmos DB Interaction 3</h1><p>' + MY_TEST + '</p>'
 
 #@app.route('/api/v1/AddItem', methods=['GET'])
-#def create_items():
-#    client    = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
-#    db        = client.get_database_client(DATABASE_ID)
-#    container = db.get_container_client(CONTAINER_ID)
+def create_items():
+    client    = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
+    db        = client.get_database_client(DATABASE_ID)
+    container = db.get_container_client(CONTAINER_ID)
 
-#    fname         = request.args['fname']
-#    lname         = request.args['lname']
-#    phone         = request.args['phone']
-#    email         = request.args['email']
-#    item_id       = fname + lname
-#    person = {'id'           : item_id,
-#              'first_name'   : fname,
-#              'last_name'    : lname,
-#              'phone'        : phone,
-#              'email'        : email
-#            }
+    fname         = request.args['fname']
+    lname         = request.args['lname']
+    phone         = request.args['phone']
+    email         = request.args['email']
+    item_id       = fname + lname
+    person = {'id'           : item_id,
+              'first_name'   : fname,
+              'last_name'    : lname,
+              'phone'        : phone,
+              'email'        : email
+            }
 
-#    container.create_item(body=person)
+    container.create_item(body=person)
 
-#    return "Person Added"
+    return "Person Added"
 
 @app.route('/api/v1/ReadItems', methods=['GET'])
 def read_items():
