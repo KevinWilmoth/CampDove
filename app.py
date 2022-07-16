9a0d6eeb952e246ad715a83e024f487f058182b0
KevinWilmoth <kevin.wilmoth@gmail.com>
2022-07-16 12:11:45 -0400

    Warnings, format, and church search

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from azure.cosmos.partition_key import PartitionKey
 import datetime
 
 import config
-import Church_Table
 import tab_table
 import transaction_table
 import camper_class
@@ -35,12 +34,20 @@ def add_tab():
     contact_name   = request.form['contact_name']
     worker_name    = request.form['worker_name']
     no_limit       = 'False'
+    
     if "no_limit" in request.form:
         no_limit = 'True'
-    weekly_limit   = request.form['weekly_limit']
-    prepaid_amount = request.form['prepaid_amount']
+    
+    if 'weekly_limit' not in request.form:
+        weekly_limit = '0.0'
+    else:
+        weekly_limit   = request.form['weekly_limit']
 
-    app.logger.info('app.add_tab noLimit = [' + no_limit + ']')
+    prepaid_amount = request.form['prepaid_amount']
+    if prepaid_amount=='':
+        prepaid_amount = '0.0'
+
+    app.logger.info('prepaid_amount = [' + prepaid_amount + ']')
 
     tab_table.add_tab(camper_fname, camper_lname, church_name, contact_name, worker_name, weekly_limit, prepaid_amount, no_limit,app)
 
@@ -105,14 +112,14 @@ def add_transaction():
     amount = request.form['transaction_amount']
     transaction_table.add_transaction(id,amount)
 
-    return redirect(url_for('tab_detail'), code=307)
+    return redirect(url_for('show_tab'), code=307)
 
 @app.route('/delete_transaction', methods=['POST'])
 def delete_transaction():
     transaction_id  = request.form['transaction_id']
     transaction_table.delete_transaction(transaction_id)
 
-    return redirect(url_for('tab_detail'), code=307)
+    return redirect(url_for('show_tab'), code=307)
 
 @app.route('/tabs', methods=['GET'])
 def tabs():
@@ -144,29 +151,21 @@ def show_tab():
     tabs = tab_table.get_tabs()
     campers              = []
 
-    id     = request.form['id']
+    id           = request.form['id']
     transactions = transaction_table.get_transactions_for_camper(id,app)
-    tab    = tab_table.get_tab(id)
+    tab          = tab_table.get_tab(id)
 
-    transactionDays    = []
-    transactionAmounts = []
-    transactionNumbers = []
-    transactionTotals  = []
-    transactionIds     = []
+    transactionDays     = []
+    transactionAmounts  = []
+    transactionNumbers  = []
+    transactionTotals   = []
+    transactionIds      = []
+    weeklyLimitWarnings = []
 
     counter            = 1
     transactionTotal   = 0
 
-    for doc in transactions:
-        transactionAmount = float(doc.get('amount'))
-        transactionTotal  = transactionTotal + transactionAmount
-        transactionDays.append(doc.get('day_of_week'))
-        transactionAmounts.append(str(transactionAmount))
-        transactionIds.append(doc.get('id'))
-        transactionNumbers.append(str(counter))
-        transactionTotals.append(str(transactionTotal))
-        counter = counter + 1
-
+        
     for doc in tabs:
         c1 = camper_class.camper(doc.get('camper_first_name'),
                                 doc.get('camper_last_name'),
@@ -181,25 +180,45 @@ def show_tab():
                    )
         campers.append(c1)
 
+    for transaction in transactions:
+        weeklyLimitWarning = 'noWarning'
+        transactionAmount = float(transaction.get('amount'))
+        transactionTotal  = transactionTotal + transactionAmount
+        transactionDays.append(transaction.get('day_of_week'))
+        transactionAmounts.append(str(transactionAmount))
+        transactionIds.append(transaction.get('id'))
+        transactionNumbers.append(str(counter))
+        transactionTotals.append(str(transactionTotal))
+        weeklyLimitWarnings.append
+        counter = counter + 1
+        if (float(transactionTotal) > tab.get('weeklyLimit')) and (tab.get('noLimit') == False):
+            weeklyLimitWarning = 'overLimit'
+        elif ((float(transactionTotal) + 5.0) >= tab.get('weeklyLimit')) and (tab.get('noLimit') == False):
+             weeklyLimitWarning = 'warning'
+
+        weeklyLimitWarnings.append(weeklyLimitWarning)
+
+            
     campers.sort(key=lambda x: (x.lname, x.fname))
 
     return render_template('show_tab.html', 
                            campers = campers,
-                           camperFirstName    = tab.get('camper_first_name'),
-                           camperLastName     = tab.get('camper_last_name'), 
-                           NoLimit            = tab.get('noLimit'),
-                           homeChurch         = tab.get('home_church'),
-                           dailyLimit         = tab.get('dailyLimit'),
-                           contactName        = tab.get('contact_name'),
-                           weeklyLimit        = tab.get('weeklyLimit'),
-                           workerName         = tab.get('workerName'),
-                           id                 = tab.get('id'),
-                           prepaidAmount      = tab.get('prepaid'),
-                           transactionDays    = transactionDays,
-                           transactionAmounts = transactionAmounts,
-                           transactionNumbers = transactionNumbers,
-                           transactionTotals  = transactionTotals,
-                           transactionIds     = transactionIds
+                           camperFirstName     = tab.get('camper_first_name'),
+                           camperLastName      = tab.get('camper_last_name'), 
+                           NoLimit             = tab.get('noLimit'),
+                           homeChurch          = tab.get('home_church'),
+                           dailyLimit          = tab.get('dailyLimit'),
+                           contactName         = tab.get('contact_name'),
+                           weeklyLimit         = tab.get('weeklyLimit'),
+                           workerName          = tab.get('workerName'),
+                           id                  = tab.get('id'),
+                           prepaidAmount       = tab.get('prepaid'),
+                           transactionDays     = transactionDays,
+                           transactionAmounts  = transactionAmounts,
+                           transactionNumbers  = transactionNumbers,
+                           transactionTotals   = transactionTotals,
+                           transactionIds      = transactionIds,
+                           weeklyLimitWarnings = weeklyLimitWarnings
                           )
 
 if __name__ == '__main__':
