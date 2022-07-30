3ffa9e095553f2d5e46e11d6e87d1784e91c8be2
KevinWilmoth <kevin.wilmoth@gmail.com>
2022-07-30 11:42:30 -0400

    Completed config.py #2

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
 import os
 
 settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://campdovetest.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY'),
-    'creator_key': os.environ.get('CAMP_DOVE_MASTER_KEY'),
-    'my_test': os.environ.get('MY_TEST'),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'CampDoveTest'),
-    'item_container_id': os.environ.get('ITEM_COSMOS_CONTAINER', 'SnackShackItem'),
-    'user_container_id': os.environ.get('USER_COSMOS_CONTAINER', 'User'),
-    'tab_container_id': os.environ.get('TAB_COSMOS_CONTAINER', 'Tab'),
-    'transaction_container_id': os.environ.get('TRANSACTION_COSMOS_CONTAINER', 'Transaction'),
-    'max_return_items': os.environ.get('MAX_RETURN_ITEMS',  1000),
-    'base_url' : os.environ.get('BASE_URL', 'http://localhost:5000/'),
+    'master_key'              : os.environ.get('ACCOUNT_KEY'),
+    'my_test'                 : os.environ.get('MY_TEST'),
+    'host'                    : os.environ.get('ACCOUNT_HOST'                 , 'https://campdovetest.documents.azure.com:443/'),
+    'database_id'             : os.environ.get('COSMOS_DATABASE'              , 'CampDoveTest'),
+    'item_container_id'       : os.environ.get('ITEM_COSMOS_CONTAINER'        , 'SnackShackItem'),
+    'user_container_id'       : os.environ.get('USER_COSMOS_CONTAINER'        , 'User'),
+    'tab_container_id'        : os.environ.get('TAB_COSMOS_CONTAINER'         , 'Tab'),
+    'transaction_container_id': os.environ.get('TRANSACTION_COSMOS_CONTAINER' , 'Transaction'),
+    'max_return_items'        : os.environ.get('MAX_RETURN_ITEMS'             ,  1000),
+    'base_url'                : os.environ.get('BASE_URL'                     , 'http://localhost:5000/'),
 }
